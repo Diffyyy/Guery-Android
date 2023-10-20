82380a51482d283231bd17f0c2d6c39dc7a5a1bb
james-patrick-kok <james_patrick_kok@dlsu.edu.ph>
2023-10-20 23:44:23 +0800

    added home fragment; fixed the navigation bar

--- app/src/main/java/com/mobdeve/s13/kok/james/gueryandroid/HomeActivity.java
+++ app/src/main/java/com/mobdeve/s13/kok/james/gueryandroid/HomeActivity.java
@@ -1,22 +1,61 @@
 package com.mobdeve.s13.kok.james.gueryandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.mobdeve.s13.kok.james.gueryandroid.databinding.ActivityHomeBinding;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnItemSelectedListener {
+
+    Fragment home = new HomeFragment();
 
     @Override
-protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
-        PostItemAdapter adapter = new PostItemAdapter(PostsGenerator.generatePosts()) ;
-        binding.postsRv.setAdapter(adapter);
-        binding.postsRv.setLayoutManager(new LinearLayoutManager(this));
-
         setContentView(binding.getRoot());
+
+        // Initialize the default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragmentContainerView, new HomeFragment())
+                    .commit();
+        }
+
+        BottomNavigationView bottomNavigationView = binding.navbar;
+        bottomNavigationView.setOnItemSelectedListener(this);
+        }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.nav_home){
+            replaceFragment(home);
+            return true;
+        }
+        else if(item.getItemId() == R.id.nav_add){
+            return true;
+        }
+        else if(item.getItemId() == R.id.nav_community){
+            return true;
+        }
+        else if(item.getItemId() == R.id.nav_profile){
+            return true;
+        }
+        else if(item.getItemId() == R.id.nav_notification){
+            return true;
+        }
+        else return false;
+    }
+
+    private void replaceFragment (Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, fragment).commit();
     }
 }
