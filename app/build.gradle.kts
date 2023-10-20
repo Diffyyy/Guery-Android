59f59e3b866d99ef94e6b0358a0d81fb37099027
BurgerTruck <burgertruck@pop-os.localdomain>
2023-10-20 19:44:47 +0800

    refactoring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,5 +44,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.8")
 }
--- app/src/main/java/com/mobdeve/s13/kok/james/gueryandroid/HomeActivity.java
+++ app/src/main/java/com/mobdeve/s13/kok/james/gueryandroid/HomeActivity.java
@@ -4,16 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 
-import com.mobdeve.s13.kok.james.gueryandroid.databinding.ActivityMainBinding;
+import com.mobdeve.s13.kok.james.gueryandroid.databinding.ActivityHomeBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
 protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
         PostItemAdapter adapter = new PostItemAdapter(PostsGenerator.generatePosts()) ;
         binding.postsRv.setAdapter(adapter);
         binding.postsRv.setLayoutManager(new LinearLayoutManager(this));
